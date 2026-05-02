d2da9529c5f5553aefe1f656aa20d6b18e532a67
Pablo Silva <pabloantoniomascena@gmail.com>
2026-05-02 16:50:27 -0300

    Update rateLimit.ts

--- src/Middlewares/rateLimit.ts
+++ src/Middlewares/rateLimit.ts
@@ -2,8 +2,8 @@ import rateLimit from 'express-rate-limit';
 import { error } from 'node:console';
 
 export const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // 5 req por ip
+  windowMs: 15 * 60 * 1000, 
+  max: 5, 
   handler: (req, res) => {
     res.status(429).json({
       error: 'Too many requests, please try again later.'
@@ -12,8 +12,3 @@ export const limiter = rateLimit({
     console.error(`Rate limit exceeded for IP: ${req.ip}`);
   }
 });
-
-// Para usarlo, simplemente importa el middleware y úsalo en tu aplicación Express
-// import { limiter } from './rateLimit';
-// app.use(limiter);
-
