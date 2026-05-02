39f3e28e5cff8e159485f651fde723b9eaaba513
Pablo Silva <pabloantoniomascena@gmail.com>
2026-05-02 16:54:38 -0300

    Update security.js

--- src/Middlewares/security.js
+++ src/Middlewares/security.js
@@ -1,10 +1,7 @@
 import helmet from "helmet";
 import xss from "xss-clean";
-//variavel
+
 export const securityMiddleware = [
-    helmet(), // adiciona automaticamente headers de segurança
-    xss(), //evita xss
+    helmet(), 
+    xss(), 
 ];
-//evita xss 
-// adiciona automaticamente headers de segurança 
-// para proteger contra ataques comuns, como clickjacking, cross-site scripting (XSS) e outros.
