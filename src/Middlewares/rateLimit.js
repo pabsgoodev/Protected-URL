432445b9a02273d96596d15d12920272c0b86bf6
Pablo Silva <pabloantoniomascena@gmail.com>
2026-05-02 16:50:58 -0300

    Update rateLimit.js

--- src/Middlewares/rateLimit.js
+++ src/Middlewares/rateLimit.js
@@ -1,8 +1,5 @@
 import rateLimit from 'express-rate-limit';
 export const limiter = rateLimit({
-    windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 100, // 100 req por ip
+    windowMs: 15 * 60 * 1000, 
+    max: 100, 
 });
-// Para usarlo, simplemente importa el middleware y úsalo en tu aplicación Express
-// import { limiter } from './rateLimit';
-// app.use(limiter);
