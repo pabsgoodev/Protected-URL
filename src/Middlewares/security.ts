171e7dd0850cb0e5b82b4c38c9c9734ec4b994ea
Pablo Silva <pabloantoniomascena@gmail.com>
2026-05-02 16:53:33 -0300

    Refactor security middleware and

--- src/Middlewares/security.ts
+++ src/Middlewares/security.ts
@@ -1,9 +1,8 @@
 import helmet from "helmet";
 import {Response, Request, NextFunction} from "express"
 
-//variavel
 export const securityMiddleware = [
-    helmet(), // adiciona automaticamente headers de segurança
+    helmet(),
 ];
  import xss from "xss";
 
@@ -17,6 +16,3 @@ export function xssMiddleware(req: Request, res: Response, next: NextFunction) {
 
     next();
 }
-//evita xss 
-// adiciona automaticamente headers de segurança 
-// para proteger contra ataques comuns, como clickjacking, cross-site scripting (XSS) e outros.
